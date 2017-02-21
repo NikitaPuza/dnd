9cc3815a2a837b643c94ab8888c4f82ee032f320
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-02-20 19:56:59 -0600

    Adding basic front end and connecting to Mongodb

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://nikitapuzanenko:p7708541@ds157439.mlab.com:57439/heroku_vxg36dwt', function (err, db) {
+    if (err) throw err
+
+    db.collection('characters').find().toArray(function (err, result) {
+        if (err) throw err
+
+        console.log(result)
+    })
+})
 
 var index = require('./routes/index');
 var users = require('./routes/users');
