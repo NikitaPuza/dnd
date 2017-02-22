056a6470cf9ef64be0b9d895171c1345cd95d154
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-02-21 19:08:37 -0600

    Removing Users route

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ MongoClient.connect('mongodb://nikitapuzanenko:p7708541@ds157439.mlab.com:57439/
 })
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -32,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
