d8b6421c09e19ef1abb876e003c49e93d27badaf
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-03-14 18:25:19 -0500

    Adding save/edit position buttons

--- public/js/snap.js
+++ public/js/snap.js
@@ -1,35 +1,45 @@
 $(document).ready(function() {
-    interact('.panel-default')
-        .draggable({
-            snap: {
-                targets: [
-                    interact.createSnapGrid({x: 20, y: 20})
-                ],
-                range: Infinity,
-                relativePoints: [{x: 0, y: 0}]
-            },
-        restrict: {
-            restriction: "parent",
-            endOnly: true,
-            elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-        },
-        autoScroll: true,
+    $('.edit-position').click(function () {
+        $('.edit-position').toggleClass('hidden')
+        $('.save-position').toggleClass('hidden')
+        interact('.panel-default')
+            .draggable({
+                snap: {
+                    targets: [
+                        interact.createSnapGrid({x: 20, y: 20})
+                    ],
+                    range: Infinity,
+                    relativePoints: [{x: 0, y: 0}]
+                },
+                restrict: {
+                    restriction: "parent",
+                    endOnly: true,
+                    elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+                },
+                autoScroll: true,
 
-        onmove: dragMoveListener,
+                onmove: dragMoveListener,
+            });
+
+        function dragMoveListener(event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.webkitTransform =
+                target.style.transform =
+                    'translate(' + x + 'px, ' + y + 'px)';
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
+
+        window.dragMoveListener = dragMoveListener;
     });
-
-    function dragMoveListener(event) {
-        var target = event.target,
-            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-        target.style.webkitTransform =
-            target.style.transform =
-                'translate(' + x + 'px, ' + y + 'px)';
-
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-    }
-
-    window.dragMoveListener = dragMoveListener;
+    $('.save-position').click(function () {
+        $('.save-position').toggleClass('hidden')
+        $('.edit-position').toggleClass('hidden')
+        interact('.panel-default')
+            .draggable(false)
+    });
 });
